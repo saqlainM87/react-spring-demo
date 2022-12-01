636baad72557d9cad5567df4e50c8455efda552f
saqlainM87 <saqlain.mahmud@monstar-lab.com>
2022-12-01 17:39:01 +0600

    feat: add useSpring example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,67 +1,69 @@
+import { Button, Card } from 'flowbite-react';
+import { useState } from 'react';
+import { animated, useSpring } from 'react-spring';
+
 export default function Home() {
+    const [toggle1, setToggle1] = useState(false);
+    const styles1 = useSpring({
+        opacity: toggle1 ? 1 : 0,
+        config: {
+            duration: 2000,
+        },
+    });
+
     return (
-        <div className="flex items-center justify-center w-full h-screen bg-gray-200">
-            <ol className="relative border-l border-gray-200 dark:border-gray-700">
-                <li className="mb-10 ml-4">
-                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700" />
-                    <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                        February 2022
-                    </time>
-                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                        Application UI code in Tailwind CSS
-                    </h3>
-                    <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-                        Get access to over 20+ pages including a dashboard
-                        layout, charts, kanban board, calendar, and pre-order
-                        E-commerce &amp; Marketing pages.
+        <div className="flex flex-col space-y-6 h-screen justify-center items-center">
+            {/* useSpring() */}
+            <Button
+                gradientDuoTone="cyanToBlue"
+                onClick={() => {
+                    setToggle1((prevState) => !prevState);
+                }}
+            >
+                <h1 className="text-xl">useSpring()</h1>
+            </Button>
+
+            <animated.div className="w-1/2 h-min" style={styles1}>
+                <Card href="#">
+                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        Div 1
+                    </h5>
+                    <p className="font-normal text-gray-700 dark:text-gray-400">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Ipsam recusandae exercitationem atque esse
+                        aspernatur illum repellat provident reiciendis?
+                        Suscipit, placeat.
                     </p>
-                    <a
-                        href="#"
-                        className="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-                    >
-                        Learn more{'{'}' '{'}'}
-                        <svg
-                            className="ml-2 w-3 h-3"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                                clipRule="evenodd"
-                            />
-                        </svg>
-                    </a>
-                </li>
-                <li className="mb-10 ml-4">
-                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700" />
-                    <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                        March 2022
-                    </time>
-                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                        Marketing UI design in Figma
-                    </h3>
-                    <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-                        All of the pages and components are first designed in
-                        Figma and we keep a parity between the two versions even
-                        as we update the project.
+                </Card>
+            </animated.div>
+
+            <animated.div className="w-1/2 h-min" style={styles1}>
+                <Card href="#">
+                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        Div 2
+                    </h5>
+                    <p className="font-normal text-gray-700 dark:text-gray-400">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Ipsam recusandae exercitationem atque esse
+                        aspernatur illum repellat provident reiciendis?
+                        Suscipit, placeat.
                     </p>
-                </li>
-                <li className="ml-4">
-                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700" />
-                    <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                        April 2022
-                    </time>
-                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                        E-Commerce UI code in Tailwind CSS
-                    </h3>
-                    <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-                        Get started with dozens of web components and
-                        interactive elements built on top of Tailwind CSS.
+                </Card>
+            </animated.div>
+
+            <animated.div className="w-1/2 h-min" style={styles1}>
+                <Card href="#">
+                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        Div 3
+                    </h5>
+                    <p className="font-normal text-gray-700 dark:text-gray-400">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Ipsam recusandae exercitationem atque esse
+                        aspernatur illum repellat provident reiciendis?
+                        Suscipit, placeat.
                     </p>
-                </li>
-            </ol>
+                </Card>
+            </animated.div>
         </div>
     );
 }
